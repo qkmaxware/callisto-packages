2dc28c2ce7ecd3b77028fc49472ac6bdbc9ee196
qkmaxware <qkmaxware@gmail.com>
2026-06-07 16:05:30 -0700

    Added tela icons

--- pkgs/callisto-theme-icons/build.sh
+++ pkgs/callisto-theme-icons/build.sh
@@ -2,7 +2,12 @@
 mkdir -p files/usr/share/icons
 git clone --depth 1 https://github.com/vinceliuice/Tela-icon-theme.git
 ./Tela-icon-theme/install.sh -c -d ./files/usr/share/icons -n "Tela (Callisto)"
-rm -rf Tela-icon-theme
+
+echo "Cleaning up temporary files..."
+rm -rf Tela-icon-theme                                              # Remove the cloned repository after installation
+rm -rf "files/usr/share/icons/Tela (Callisto)/icon-theme.cache"     # Remove the cache as its generated on the target system
+rm -rf "files/usr/share/icons/Tela (Callisto)/scalable@2x"          # Remove the scalable@2x symbolic link
+ln -s "/usr/share/icons/Tela (Callisto)/scalable" "files/usr/share/icons/Tela (Callisto)/scalable@2x"    # Create a new symbolic link for scalable@2x pointing to scalable
 ls -al "files/usr/share/icons/Tela (Callisto)"
 
 # Customize the icons for Callisto OS

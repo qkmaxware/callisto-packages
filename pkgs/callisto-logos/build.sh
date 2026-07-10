eb3a19e7b79430350ebe244269e0a8271288465e
qkmaxware <qkmaxware@gmail.com>
2026-07-09 17:47:54 -0700

    Trying to get callisto logos to replace system-logos and fedora-logos

--- pkgs/callisto-logos/build.sh
+++ pkgs/callisto-logos/build.sh
@@ -11,9 +11,9 @@ mkdir -p files/usr/share/anaconda/pixmaps/
 cp -f anaconda/pixmaps/anaconda_header.png files/usr/share/anaconda/pixmaps/anaconda_header.png
 cp -f anaconda/pixmaps/progress_first.png files/usr/share/anaconda/pixmaps/progress_first.png
 cp -f anaconda/pixmaps/sidebar-bg.png files/usr/share/anaconda/pixmaps/sidebar-bg.png
-magick -background none -density 300 callisto-logo.svg -resize 128x128 files/usr/share/anaconda/pixmaps/sidebar-logo.png
+convert -background none -density 300 callisto-logo.svg -resize 128x128 files/usr/share/anaconda/pixmaps/sidebar-logo.png
 cp -f anaconda/pixmaps/progress_first.png files/usr/share/anaconda/pixmaps/splash.png
-cp -f firstboot/pixmaps/topbar-bg.png files/usr/share/anaconda/pixmaps/topbar-bg.png
+cp -f anaconda/pixmaps/topbar-bg.png files/usr/share/anaconda/pixmaps/topbar-bg.png
 
 mkdir -p files/usr/share/firstboot/themes/generic
 cp -f firstboot/firstboot-left.png files/usr/share/firstboot/themes/generic/firstboot-left.png
@@ -25,29 +25,29 @@ mkdir -p files/usr/share/icons/hicolor/48x48/apps/
 cp -f callisto-logo.svg files/usr/share/icons/hicolor/scalable/apps/distribution-logo.svg
 cp -f callisto-logo.svg files/usr/share/icons/hicolor/scalable/apps/callisto-logo.svg
 cp -f callisto-logo.svg files/usr/share/icons/hicolor/scalable/apps/anaconda.svg
-magick -background none -density 300 callisto-logo.svg -resize 49x49 files/usr/share/icons/hicolor/48x48/apps/anaconda.png
+convert -background none -density 300 callisto-logo.svg -resize 49x49 files/usr/share/icons/hicolor/48x48/apps/anaconda.png
 cp -f callisto-logo.svg files/usr/share/icons/hicolor/scalable/apps/fedora-logo-icon.svg
 cp -f callisto-logo.svg files/usr/share/icons/hicolor/scalable/apps/fedora-logo-sprite.svg
 cp -f start-here.svg files/usr/share/icons/hicolor/scalable/apps/start-here.svg
 cp -f start-here.svg files/usr/share/icons/hicolor/scalable/places/start-here.svg
 
 mkdir -p files/usr/share/icons/oxygen/48x48/apps/
-magick -background none -density 300 callisto-logo.svg -resize 49x49 files/usr/share/icons/oxygen/48x48/apps/anaconda.png
+convert -background none -density 300 callisto-logo.svg -resize 49x49 files/usr/share/icons/oxygen/48x48/apps/anaconda.png
 
 mkdir -p files//usr/share/kde4/apps/ksplash/Themes/Leonidas/2048x1536
 cp -f ksplash/logo.png files/usr/share/kde4/apps/ksplash/Themes/Leonidas/2048x1536/logo.png
 
 mkdir -p files//usr/share/pixmaps/bootloader
-magick -background none -density 300 callisto-logo.svg -resize 128x128 files/usr/share/pixmaps/bootloader/fedora.icns
+convert -background none -density 300 callisto-logo.svg -resize 128x128 files/usr/share/pixmaps/bootloader/fedora.icns
 cp -f fedora.vol files/usr/share/pixmaps/bootloader/fedora.vol
 cp -f fedora-media.vol files/usr/share/pixmaps/bootloader/fedora-media.vol
 
 mkdir -p files/usr/share/pixmaps
 mkdir -p files/usr/share/pixmaps/splash
-magick -background none -density 300 callisto-logo.svg -resize 128x128 files/usr/share/pixmaps/fedora-logo-small.png
+convert -background none -density 300 callisto-logo.svg -resize 128x128 files/usr/share/pixmaps/fedora-logo-small.png
 cp -f callisto-logo.svg files/usr/share/pixmaps/fedora-logo-sprite.svg
-magick -background none -density 300 callisto-logo.svg -resize 240x240 files/usr/share/pixmaps/fedora-logo.png
-magick -background none -density 300 callisto-logo.svg -resize 240x240 files/usr/share/pixmaps/system-logo-white.png
+convert -background none -density 300 callisto-logo.svg -resize 240x240 files/usr/share/pixmaps/fedora-logo.png
+convert -background none -density 300 callisto-logo.svg -resize 240x240 files/usr/share/pixmaps/system-logo-white.png
 cp -f splash/gnome-splash.png files/usr/share/pixmaps/splash/gnome-splash.png
 
 mkdir -p files/usr/share/plymouth/themes/charge
@@ -85,19 +85,19 @@ cp -f progress/progress-30.png files/usr/share/plymouth/themes/charge/progress-3
 cp -f progress/progress-31.png files/usr/share/plymouth/themes/charge/progress-31.png
 cp -f progress/progress-32.png files/usr/share/plymouth/themes/charge/progress-32.png
 
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-00.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-01.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-02.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-03.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-04.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-05.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-06.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-07.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-08.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-09.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-10.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-11.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-12.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-13.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-14.png
-magick -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-15.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-00.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-01.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-02.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-03.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-04.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-05.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-06.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-07.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-08.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-09.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-10.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-11.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-12.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-13.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-14.png
+convert -background none -density 300 callisto-logo.svg -resize 135x135 files/usr/share/plymouth/themes/charge/throbber-15.png
